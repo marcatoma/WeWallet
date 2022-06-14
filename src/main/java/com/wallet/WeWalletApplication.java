01a2099819460c959373e48d33124f614cdd14e5
marcatoma <christian.marcatoma.est@tecazuay.edu.ec>
2022-06-14 15:21:31 -0500

    commit

---
+++ src/main/java/com/wallet/WeWalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeWalletApplication.class, args);
+	}
+
+}
