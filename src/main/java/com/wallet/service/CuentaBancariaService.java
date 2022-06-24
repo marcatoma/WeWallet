63930d1ee75078ece229be0151da95ec6934352b
marcatoma <christian.marcatoma.est@tecazuay.edu.ec>
2022-06-23 22:11:22 -0500

    repositorio

---
+++ src/main/java/com/wallet/service/CuentaBancariaService.java
@@ -0,0 +1,28 @@
+package com.wallet.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wallet.models.CuentaBancaria;
+import com.wallet.models.Usuario;
+import com.wallet.repository.ICuentaBancaria;
+
+@Service
+public class CuentaBancariaService {
+	
+	@Autowired private ICuentaBancaria cuentaRepo;
+	
+	@Transactional
+	public CuentaBancaria RegisterCuentaBancaria(CuentaBancaria cuenta) {
+		return cuentaRepo.save(cuenta);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<CuentaBancaria> GetUserAccounts(Usuario user){
+		return null;
+	}
+	
+}
